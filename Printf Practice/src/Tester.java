13ef892864f48254d4b1e2660f944c1d53bc6bf9
mcguire <mcguire@mullenhigh.com>
2016-11-02 10:02:38 -0600

    Initial commit

---
+++ Printf Practice/src/Tester.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+
+public class Tester
+	{
+		public static void main(String[] args)
+			{
+			String s1 = "Mike";
+			String s2 = "Phillip";
+			String s3 = "Christopher";
+			String s4 = "Joe";
+			
+			ArrayList names = new ArrayList();
+			names.add(s1);
+			names.add(s2);
+			names.add(s3);
+			names.add(s4);
+			
+			System.out.println(s1 + "\t" + s2 + "\t" 
+			+ s3 + "\t" + s4);
+			System.out.println();
+			
+			System.out.printf("%-12s %-12s %-12s %-12s", 
+					s1, s2, s3, s4);
+			System.out.println();
+			System.out.println();
+		
+			for (Object s : names)
+				{
+				System.out.printf("%-12s", s);
+				}
+			}
+	}
