2b1de3647e955b20dd37f180f25831ee1bc41837
Jack Paul <jackrpaul13@gmail.com>
2017-12-01 14:28:54 -0700

    correct

--- Printf Practice/src/Tester.java
+++ Printf Practice/src/Tester.java
@@ -8,19 +8,21 @@ public class Tester
 			String s2 = "Phillip";
 			String s3 = "Christopher";
 			String s4 = "Joe";
+			String s6 = "Liam";
 			//test test
 			ArrayList<String> names = new ArrayList<String>();
 			names.add(s1);
 			names.add(s2);
 			names.add(s3);
 			names.add(s4);
+			names.add(s6);
 			
 			System.out.println(s1 + "\t" + s2 + "\t" 
 			+ s3 + "\t" + s4);
 			System.out.println();
 			
 			System.out.printf("%-12s %-12s %-12s %-12s", 
-					s1, s2, s3, s4);
+					s1, s2, s3, s4, s6);
 
 			System.out.println("\n");
 
